244e5e7064c3a0d406c31aa295438b1c29886dec
System Administrator <root@Chases-MacBook-Pro.local>
2017-02-26 11:27:08 -0600

    added sql boiler plate

--- signup.php
+++ signup.php
@@ -9,9 +9,11 @@
 
 	<body>
 		<form action="thanks.php" method="POST">
-			Enter your username in the form of an email address:<input id="user" type="text" name="user"></input></br>
-			Enter your password:<input ng-model="pass" id="pass" type="password" name="pass"></input></br>
-			Confirm your password:<input ng-model="conf" id="pass" type="password" name="pass"></input></br>
+			Enter your first name:<input id="user" type="text" name="fname"></input></br>
+			Enter your last name:<input id="user" type="text" name="lname"></input></br>
+			Enter your username in the form of an email address:<input id="user" type="text" name="email"></input></br>
+			Enter your password:<input ng-model="pass" type="password" name="pass"></input></br>
+			Confirm your password:<input ng-model="conf" type="password" name="pass"></input></br>
 			<input id="submit" type="submit" ></input>
 		</form>
 	</body>
